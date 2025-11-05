98a2f6d6026e4fe5a1b4d6bea6f90ae088660e80
jainkgaurav <jainkgaurav@gmail.com>
2025-11-06 07:34:39 +0800

    Create symbol_trades.py

---
+++ symbol_trades.py
@@ -0,0 +1,12 @@
+[{'symbol': 'ZENUSDT', 
+'status': 'open',
+ 'upperband': 20.288819999999998, 
+'lowerband': 19.891, 'no_of_trades': 3, 
+'open_timestamp': 
+'2025-11-05T15:24:37.974032',
+ 'close_timestamp': None,
+ 'max_balance': 98.72660632,
+ 'strategy': 'RSIReTest',
+ 'is_sl_open': 1,
+ 'is_tp_open': 1,
+ 'sl_factor': 2, 'tp_factor': 15, 'unRealizedProfit': 0.0, 'TradeSize': 96.4487295}]
